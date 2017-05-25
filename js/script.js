ea63e42068c166c2b4cd204ac50dccab52da129e
Dorothy Shyamala <dorothyshyamala@Dorothys-MacBook-Air.local>
2017-05-25 14:59:55 +0530

    modified

---
+++ js/script.js
@@ -0,0 +1,10 @@
+        $(".owl-carousel").owlCarousel({
+            items :5,
+            pagination : false,
+            margin: 5,
+            nav:true,
+            navText: [
+            "<i class='mdi mdi-chevron-left'><</i>",
+            "<i class='mdi mdi-chevron-right'>></i>"
+            ],
+        });
